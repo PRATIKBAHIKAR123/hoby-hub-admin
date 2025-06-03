793add296e9698466a61e61d16fdb5f109285f83
Mohammed <mohammedjuzer5252@gmail.com>
2025-06-03 15:12:36 +0530

    added img url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://api.hobyhub.com/api/1',
+  imageUrl: 'https://api.hobyhub.com',
   username: '',
   password: '',
   defaultauth: 'fackbackend',
